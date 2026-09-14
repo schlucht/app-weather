15c8919384f1fd61ce4d3ba485f2e7c9b93daef0
Schmid Lothar <web@jagolo.ch>
2026-09-14 11:13:54 +0200

    Added weather.css

---
+++ weatherApp/src/app/pages/weather/weather.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-weather',
+  styleUrl: './weather.css',
+  templateUrl: './weather.html',
+})
+export class Weather {
+}
