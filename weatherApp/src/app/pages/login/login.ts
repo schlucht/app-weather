95b0e93cf93546d70b6c05fc3e0c80a8edcec1e3
Schmid Lothar <web@jagolo.ch>
2026-09-14 11:12:14 +0200

    Added login.html

---
+++ weatherApp/src/app/pages/login/login.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-login',
+  styleUrl: './login.css',
+  templateUrl: './login.html',
+})
+export class Login {
+}
