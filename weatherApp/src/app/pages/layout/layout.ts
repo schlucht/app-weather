5ecab42203da608e60bd5c78b313f292e5898780
Schmid Lothar <web@jagolo.ch>
2026-09-14 11:40:46 +0200

    DELETE - ALL

--- weatherApp/src/app/pages/layout/layout.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  imports: [],
-  selector: 'app-layout',
-  styleUrl: './layout.css',
-  templateUrl: './layout.html',
-})
-export class Layout {}
--- weatherApp/src/environments/environment.ts
+++
@@ -1,3 +0,0 @@
-export const environment = {
-  "API": "632fcfa10fc8d384c57f7d2f70b7192b",
-};
