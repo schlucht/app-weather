fa519de10d9449345f9f86cc200b558a1c2dbb58
Schmid Lothar <web@jagolo.ch>
2026-09-14 11:13:02 +0200

    Added dashboard.ts

---
+++ weatherApp/src/app/pages/dashboard/dashboard.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-dashboard',
+  styleUrl: './dashboard.css',
+  templateUrl: './dashboard.html',
+})
+export class Dashboard {
+}
