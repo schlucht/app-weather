b1da89c15e97690b1da26acd4337f216eee27c7a
Schmid Lothar <web@jagolo.ch>
2026-09-14 11:12:35 +0200

    Added layout.ts

---
+++ weatherApp/src/app/pages/layout/layout.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-layout',
+  styleUrl: './layout.css',
+  templateUrl: './layout.html',
+})
+export class Layout {
+}
