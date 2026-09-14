00b113da95db72c8c779fcc020af15f8beb4156c
Schmid Lothar <web@jagolo.ch>
2026-09-14 11:27:45 +0200

    Updated app.routes.ts

--- weatherApp/src/app/app.routes.ts
+++ weatherApp/src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Layout } from './pages/layout/layout';
+import { Dashboard } from './pages/Dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "/login",
+    exactMatch: 'full',
+  },
+  {
+    path: "login",
+    component: Login,
+  },
+  {
+    path: '',
+    component: Layout,
+    children: [
+      {
+        path: '/dashboar',
+        component: Dashboard
+      }
+    ]
+  }
+];
