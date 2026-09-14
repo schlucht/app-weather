465f7df29a9fd2df99f2685b231d0a02586a0d95
schlucht <web@jagolo.ch>
2026-09-14 11:35:46 +0200

    deleted all

--- weatherApp/src/app/pages/weather/weather.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  imports: [],
-  selector: 'app-weather',
-  styleUrl: './weather.css',
-  templateUrl: './weather.html',
-})
-export class Weather {}
